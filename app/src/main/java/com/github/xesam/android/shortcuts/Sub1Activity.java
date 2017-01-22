6ad21fd9760c366056370297f05d5db3eabd6be5
xesam <xesamguo@gmail.com>
2017-01-22 16:21:51 +0800

    init

---
+++ app/src/main/java/com/github/xesam/android/shortcuts/Sub1Activity.java
@@ -0,0 +1,17 @@
+package com.github.xesam.android.shortcuts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xesamguo@gmail.com on 17-1-22.
+ */
+
+public class Sub1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+    }
+}
